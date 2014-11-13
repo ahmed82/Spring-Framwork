6b2261ec10217b1e384120220c9de44fe84ddfff
Ahmed Al Salih <a.alsalih2@gmail.com>
2014-11-12 23:51:58 -0500

    Create the Order Domain Objuct to hold the data for a particular order

---
+++ OE Project/src/com/simplesoft/oe/domain/Order.java
@@ -0,0 +1,37 @@
+package com.simplesoft.oe.domain;
+
+import java.util.Date;
+
+public class Order {
+	
+	private long id;
+	private String customerName;
+	private Date orderDate;
+	private String product;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	public Date getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(Date orderDate) {
+		this.orderDate = orderDate;
+	}
+	public String getProduct() {
+		return product;
+	}
+	public void setProduct(String product) {
+		this.product = product;
+	}
+
+}
