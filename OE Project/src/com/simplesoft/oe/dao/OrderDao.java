b0882aa1b2428eac778d731ee889e4dd4c1a2aac
Ahmed Al Salih <a.alsalih2@gmail.com>
2014-11-12 23:59:42 -0500

    Create date access object interface for Order

---
+++ OE Project/src/com/simplesoft/oe/dao/OrderDao.java
@@ -0,0 +1,10 @@
+package com.simplesoft.oe.dao;
+
+import com.simplesoft.oe.domain.Order;
+
+//Date access object interface for Order
+
+public interface OrderDao {
+	public void addOrder (Order order);
+
+}
